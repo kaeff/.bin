7e7646525e3b1eaf846bbc1fa1634163c4ab1bd1
Klaus Fl <hi@kaeff.net>
2022-07-14 10:25:42 +0200

    add nvm completion

--- nvm_init.sh
+++ nvm_init.sh
@@ -1,7 +1,7 @@
 # To activate, use:
 # source ~/bin/nvm_init.sh
 
-export NVM_DIR="$HOME/.nvm"
-. "/usr/local/opt/nvm/nvm.sh"
-
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 

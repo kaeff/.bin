69fb513b31c744227b514a158b968f0c769ff628
kaeff <hi@kaeff.net>
2023-04-29 08:47:04 +0200

    prefix_filename_date_ocr.py: OCR pdf if neccessary

--- prefix_filename_date_ocr.py
+++ prefix_filename_date_ocr.py
@@ -6,10 +6,11 @@ import glob
 import re
 from pdfminer.high_level import extract_text
 from dateutil import parser
+import ocrmypdf
 
 
 # Map German month names to English
-MONTH_MAP = {
+MONTH_GERMAN_TO_ENGLISH = {
     "Januar": "January",
     "Februar": "February",
     "März": "March",
@@ -27,15 +28,15 @@ MONTH_MAP = {
 
 def translate_month_names(text):
     # Replace German month names with English
-    for month in MONTH_MAP:
-        text = text.replace(month, MONTH_MAP[month])
+    for month in MONTH_GERMAN_TO_ENGLISH:
+        text = text.replace(month, MONTH_GERMAN_TO_ENGLISH[month])
     return text
 
 
 def filter_for_date(text):
     date_text = ""
-    pattern='([0-9]{2})[\.-]([0-9]{2})[\.-]([0-9]{2,4})'
-    pattern_long='([0-9]{2})\.([a-zA-Z]+)\.([0-9]{4})'
+    pattern='([0-9]{2})[_\.-]([0-9]{2})[_\.-]([0-9]{2,4})'
+    pattern_long='([0-9]{2})\. ?([a-zA-Z]+)[ \.]([0-9]{4})'
 
     match = re.search(pattern, text)
     if match:
@@ -48,8 +49,6 @@ def filter_for_date(text):
 
 
 def extract_date(text):
-    # Translate German month names to English
-
     text = filter_for_date(text)
     text = translate_month_names(text)
 
@@ -63,12 +62,34 @@ def extract_date(text):
         return None
 
 
-def get_new_filename(pdf_file):
-    # Extract text from PDF
-    text = extract_text(pdf_file)
+def perform_ocr(pdf_file):
+    ocrmypdf.configure_logging(verbosity=-1)
+    print("Performing OCR for: " + pdf_file)
 
-    # Extract date from text
-    date = extract_date(text)
+    dirname, filename = os.path.split(pdf_file)
+    basename, ext = os.path.splitext(filename)
+    tmp_pdf_ocred = os.path.join(dirname, basename + ".ocr" + ext)
+    
+    if os.path.exists(tmp_pdf_ocred):
+        os.remove(tmp_pdf_ocred)
+    
+    ocrmypdf.ocr(pdf_file, tmp_pdf_ocred, force_ocr=True)
+    text = extract_text(tmp_pdf_ocred)
+    
+    os.remove(tmp_pdf_ocred)
+    return text
+
+
+def get_new_filename(pdf_file, print_text):
+    # Check if filename contains a date
+    date = extract_date(pdf_file)
+    if not date:
+        text = extract_text(pdf_file)
+        date = extract_date(text)
+
+        if not date:
+            text = perform_ocr(pdf_file)
+            date = extract_date(text)
 
     # Rename file with prefix and date
     if date:
@@ -79,16 +100,15 @@ def get_new_filename(pdf_file):
     return new_file_name
 
 
-def prefix_filename_date_ocr(force):
+def prefix_filename_date_ocr(force, print_text):
     # Get all PDF files in current directory
     pdf_files = glob.glob("*.PDF") + glob.glob("*.pdf")
 
     for pdf_file in pdf_files:
         # Get new file name
-        new_file_name = get_new_filename(pdf_file)
+        new_file_name = get_new_filename(pdf_file, print_text)
 
         # Rename file
-        # os.rename(pdf_file, new_file_name)
         if force:
             os.rename(pdf_file, new_file_name)
             print ("Renamed: " + new_file_name)
@@ -100,10 +120,11 @@ def prefix_filename_date_ocr(force):
 if __name__ == "__main__":
     arg_parser = ArgumentParser()
     arg_parser.add_argument("-f", "--force", action=BooleanOptionalAction, help="Actually rename files")
+    arg_parser.add_argument("-t", "--print-text", action=BooleanOptionalAction, help="Print OCR text for debugging")
     args = arg_parser.parse_args()
 
     if not args.force:
         print("DRY RUN - no files will be renamed. Call script with argument --force to rename files")
         print()
 
-    prefix_filename_date_ocr(args.force)
+    prefix_filename_date_ocr(args.force, args.print_text)
